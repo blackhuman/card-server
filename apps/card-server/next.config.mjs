e1dcfe26a67480ed8b927fe4c08199141110a347
baiyizhe <baiyizhe@meituan.com>
2025-02-06 17:43:25 +0800

    update

--- apps/card-server/next.config.mjs
+++ apps/card-server/next.config.mjs
@@ -1,4 +1,8 @@
 import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
